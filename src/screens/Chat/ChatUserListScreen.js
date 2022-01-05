22897ae0deee7e9f7ae1a1956bbe936c4269e260
muhlishabibpradana <muhlis123>
2022-01-06 00:07:56 +0700

    chat

--- src/screens/Chat/ChatUserListScreen.js
+++ src/screens/Chat/ChatUserListScreen.js
@@ -1,31 +1,21 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   FlatList,
-  BackHandler,
   Image,
-  ActivityIndicator,
   ImageBackground,
 } from 'react-native';
-import Styled from 'styled-components';
-import Moment from 'moment';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 
 import Text from '@src/components/Text';
-import Color from '@src/components/Color';
+import { useColor } from '@src/components/Color';
 import Header from '@src/components/Header';
 import TouchableOpacity from '@src/components/Button/TouchableDebounce';
 
 import Client from '@src/lib/apollo';
 import {queryLike} from '@src/lib/query';
-
 import {queryGetUserOrganizationRef} from '@src/lib/query';
-
-const MainView = Styled(View)`
-    width: 100%;
-    height: 100%;
-`;
+import { Scaffold } from 'src/components';
 
 const ChatUserListScreen = ({navigation, route}) => {
   const [itemData, setItemData] = useState({
@@ -35,6 +25,8 @@ const ChatUserListScreen = ({navigation, route}) => {
     loadNext: false,
   });
   const [selected, setSelected] = useState([]);
+
+  const { Color } = useColor();
 
   useEffect(() => {
     if (itemData.page !== -1) {
@@ -69,18 +61,19 @@ const ChatUserListScreen = ({navigation, route}) => {
         console.log(res, 'ressss');
 
         const data = res.data.getUserOrganizationRef;
-        console.log(data, 'ini dataaaaa');
-        if (data) {
-          const newData = compareData(itemData.data.concat(data));
-          console.log(newData, 'ini newww');
 
-          setStateItemData({
-            data: newData,
-            loading: false,
-            page: data.length === 10 ? itemData.page + 1 : -1,
-            loadNext: false,
-          });
+        let newArr = [];
+
+        if (data) {
+          newArr = compareData(itemData.data.concat(data));
         }
+
+        setStateItemData({
+          data: newArr,
+          loading: false,
+          page: data.length === 10 ? itemData.page + 1 : -1,
+          loadNext: false,
+        });
       })
       .catch(err => {
         console.log(err, 'errrrr');
@@ -128,87 +121,38 @@ const ChatUserListScreen = ({navigation, route}) => {
   };
 
   return (
-    <MainView>
-      <Header
-        title="Buat Room Chat"
-        actions={
-          selected.length > 0 ? (
-            <TouchableOpacity
-              style={{flex: 1, justifyContent: 'center', paddingHorizontal: 16}}
-              onPress={() => {
-                navigation.navigate('ChatDetailScreen', {
-                  roomId: null,
-                  selected,
-                });
-              }}>
-              <MaterialIcons name="send" color={Color.secondary} size={22} />
-            </TouchableOpacity>
-          ) : null
-        }
-      />
-
-      {itemData.loading ? (
-        <ActivityIndicator
-          size="large"
-          color={Color.primary}
-          style={{marginTop: 24}}
+    <Scaffold
+      fallback={itemData.loading}
+      header={
+        <Header
+          title="Buat Room Chat"
+          actions={
+            selected.length > 0 ? (
+              <TouchableOpacity
+                style={{flex: 1, justifyContent: 'center', paddingHorizontal: 16}}
+                onPress={() => {
+                  navigation.navigate('ChatDetailScreen', {
+                    roomId: null,
+                    selected,
+                  });
+                }}>
+                <MaterialIcons name="send" color={Color.secondary} size={22} />
+              </TouchableOpacity>
+            ) : null
+          }
         />
-      ) : (
-        <FlatList
-          keyExtractor={(item, index) => item.toString() + index}
-          data={itemData.data}
-          contentContainerStyle={{paddingHorizontal: 16, paddingTop: 8}}
-          renderItem={({item}) => {
-            const isSelected = selected.filter(
-              e => e.userId === item.userId,
-            )[0];
+      }
+    >
+      <FlatList
+        keyExtractor={(item, index) => item.toString() + index}
+        data={itemData.data}
+        contentContainerStyle={{paddingHorizontal: 16, paddingTop: 8}}
+        renderItem={({item}) => {
+          const isSelected = selected.filter(
+            e => e.userId === item.userId,
+          )[0];
 
-            if (isSelected) {
-              return (
-                <TouchableOpacity
-                  onPress={() => onPress(item)}
-                  onLongPress={() => onSelected(item)}
-                  style={{
-                    height: 70,
-                    width: '100%',
-                    borderRadius: 4,
-                    backgroundColor: Color.theme,
-                    marginBottom: 8,
-                    flexDirection: 'row',
-                    justifyContent: 'flex-start',
-                    alignItems: 'center',
-                  }}>
-                  <ImageBackground
-                    source={{uri: item.image}}
-                    style={{
-                      width: 50,
-                      height: 50,
-                      borderRadius: 25,
-                      marginRight: 8,
-                      backgroundColor: Color.border,
-                    }}
-                    imageStyle={{borderRadius: 30}}>
-                    <MaterialIcons
-                      name="check-circle"
-                      color={Color.success}
-                      size={22}
-                    />
-                  </ImageBackground>
-                  <View
-                    style={{
-                      height: 60,
-                      alignItems: 'flex-start',
-                      justifyContent: 'space-around',
-                    }}>
-                    <Text size={12} type="semibold" numberOfLines={1}>
-                      {item.fullname}
-                    </Text>
-                    {/* <Text size={10}>Available</Text> */}
-                  </View>
-                </TouchableOpacity>
-              );
-            }
-
+          if (isSelected) {
             return (
               <TouchableOpacity
                 onPress={() => onPress(item)}
@@ -216,44 +160,88 @@ const ChatUserListScreen = ({navigation, route}) => {
                 style={{
                   height: 70,
                   width: '100%',
+                  borderRadius: 4,
+                  backgroundColor: Color.theme,
                   marginBottom: 8,
                   flexDirection: 'row',
-                  justifyContent: 'space-between',
+                  justifyContent: 'flex-start',
                   alignItems: 'center',
                 }}>
-                <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                  <Image
-                    source={{uri: item.image}}
-                    style={{
-                      width: 50,
-                      height: 50,
-                      borderRadius: 25,
-                      marginRight: 8,
-                      backgroundColor: Color.border,
-                    }}
+                <ImageBackground
+                  source={{uri: item.image}}
+                  style={{
+                    width: 50,
+                    height: 50,
+                    borderRadius: 25,
+                    marginRight: 8,
+                    backgroundColor: Color.border,
+                  }}
+                  imageStyle={{borderRadius: 30}}>
+                  <MaterialIcons
+                    name="check-circle"
+                    color={Color.success}
+                    size={22}
                   />
-                  <View
-                    style={{
-                      height: 60,
-                      alignItems: 'flex-start',
-                      justifyContent: 'space-around',
-                    }}>
-                    <Text size={12} type="semibold" numberOfLines={1}>
-                      {item.fullname}
-                    </Text>
-                    {/* <Text size={10}>Available</Text> */}
-                  </View>
+                </ImageBackground>
+                <View
+                  style={{
+                    height: 60,
+                    alignItems: 'flex-start',
+                    justifyContent: 'space-around',
+                  }}>
+                  <Text size={12} type="semibold" numberOfLines={1}>
+                    {item.fullname}
+                  </Text>
+                  {/* <Text size={10}>Available</Text> */}
                 </View>
               </TouchableOpacity>
             );
-          }}
-          onEndReached={() =>
-            itemData.page !== -1 && setStateItemData({loadNext: true})
           }
-          onEndReachedThreshold={0.3}
-        />
-      )}
-    </MainView>
+
+          return (
+            <TouchableOpacity
+              onPress={() => onPress(item)}
+              onLongPress={() => onSelected(item)}
+              style={{
+                height: 70,
+                width: '100%',
+                marginBottom: 8,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}>
+              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Image
+                  source={{uri: item.image}}
+                  style={{
+                    width: 50,
+                    height: 50,
+                    borderRadius: 25,
+                    marginRight: 8,
+                    backgroundColor: Color.border,
+                  }}
+                />
+                <View
+                  style={{
+                    height: 60,
+                    alignItems: 'flex-start',
+                    justifyContent: 'space-around',
+                  }}>
+                  <Text size={12} type="semibold" numberOfLines={1}>
+                    {item.fullname}
+                  </Text>
+                  {/* <Text size={10}>Available</Text> */}
+                </View>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+        onEndReached={() =>
+          itemData.page !== -1 && setStateItemData({loadNext: true})
+        }
+        onEndReachedThreshold={0.3}
+      />
+    </Scaffold>
   );
 };
 
