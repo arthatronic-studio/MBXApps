dbbbb076b309ad9a32a120bc13cba15d39c23c6e
riyansghrt <riyansugiharto96@gmail.com>
2022-03-01 15:20:53 +0700

    state survey

--- src/screens/Survey/SurveyFirst.js
+++ src/screens/Survey/SurveyFirst.js
@@ -42,40 +42,61 @@ const Content = Styled(View)`
     padding: 12px
     borderRadius: 8px
 `;
-const instance =  axios.create({ baseURL: 'http://panel.sw.tribesocial.id', headers: { 'Content-Type': 'application/x-www-form-urlencoded'} });
 
 const SurveyFirst = ({navigation}) => {
     const user = useSelector((state) => state['user.auth'].login.user);
 	const loading = useSelector((state) => state['user.auth'].loading);
 
 	const [ loadingProps, showLoading, hideLoading ] = useLoading();
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [namePetugas, setNamePetugas] = useState('');
+    const [phonePetugas, setPhonePetugas] = useState('');
+    const [nameKoor, setNameKoor] = useState('');
+    const [phoneKoor, setPhoneKoor] = useState('');
 	const { Color } = useColor();
 
 	useEffect(() => {
-        getDetail()
+        submit()
     }, []);
 
-    const getDetail = async () => {
+    const submit = async () => {
+        const label = ['name', 'phone', 'email', 'namePetugas', 'phonePetugas', 'nameKoor', 'phoneKoor']
+        const dataState = [name, phone, email, namePetugas, phonePetugas, nameKoor, phoneKoor]
+        let tempData = []
+        label.forEach((element, index) => {
+            tempData.push({
+                block: '1',
+                index: index,
+                name: element,
+                value: dataState[index]
+            })
+        });
+        console.log(tempData)
         const dataq = {
-            "auth": "sha1("+"SURVEY-20220229-163729"+moment().format('YYYY-MM-DD HH:mm:ss')+")", "survey_code": "SURVEY-20220229-163729"+moment().format('YYYY-MM-DD HH:mm:ss'), "timestamps": moment().format('YYYY-MM-DD HH:mm:ss'),
-            "data":[{
-                "block": "1",
-                "index": "0",
-                "name": "Nama Penjual",
-                "value": "Riyan Sugih"
-            },
-            {
-                "block": "1",
-                "index": "1",
-                "name": "Lama Berjualan",
-                "value": "10 tahun"
-            }]
+            "auth": "sha1("+"SURVEY-20220229-163729"+moment().format('YYYY-MM-DD HH:mm:ss')+")", 
+            "survey_code": "SURVEY-20220229", 
+            "timestamps": moment().format('YYYY-MM-DD HH:mm:ss'),
+            "data": tempData
         }
         console.log(dataq, 'dataq')
-
-        const response = await instance.post('/submit-survey', dataq);
-        console.log(response, 'response')
-        
+        try {
+            const response = await axios({
+                baseURL: 'http://panel.sw.tribesocial.id',
+                method: 'post',
+                url: '/submit-survey',
+                data: dataq,
+                headers: {
+                    Accept: 'application/json'
+                },
+                timeout: 5000,
+                
+              });
+              console.log(response, "respon apicall")
+          } catch (error) {
+            console.log(error.response, 'error apicall')
+          }
       };
 
   return (
@@ -95,14 +116,21 @@ const SurveyFirst = ({navigation}) => {
                 <Text style={{fontSize: 10, fontWeight: 'bold', marginVertical: 10}}>Informasi Pribadi</Text>
                 <View style={{width: '100%'}}>
                     <TextInput placeholder='Adang Susanyo' style={{borderWidth: 1, borderColor: Color.border,
-                        width: '100%', borderRadius: 5, paddingHorizontal: 10, paddingTop: 20, height: 47}}></TextInput>
+                        width: '100%', borderRadius: 5, paddingHorizontal: 10, paddingTop: 20, height: 47}}
+                        onChangeText={(value) => setName(value)}
+                        value={name}
+                    />
                     <Text style={{fontSize: 8, color: Color.secondary, position: 'absolute', paddingHorizontal: 10, paddingVertical: 5}}>Nama Responden</Text>
                 </View>
             </View>
             <View style={{alignItems: 'flex-start', marginHorizontal: 10, marginVertical: 5}}>
                 <View style={{width: '100%'}}>
                     <TextInput placeholder='813-1234-5678' style={{borderWidth: 1, borderColor: Color.border,
-                        width: '100%', borderRadius: 5, paddingHorizontal: 40, paddingTop: 20, height: 47}}></TextInput>
+                        width: '100%', borderRadius: 5, paddingHorizontal: 40, paddingTop: 20, height: 47}}
+                        keyboardType='number-pad'
+                        onChangeText={(value) => setPhone(value)}
+                        value={phone}
+                    />
                     <Text style={{position: 'absolute', marginVertical: 17, marginHorizontal: 10, fontSize: 13}}>+62</Text>
                     <Text style={{fontSize: 8, color: Color.secondary, position: 'absolute', paddingHorizontal: 10, paddingVertical: 5}}>No Telepon</Text>
                 </View>
@@ -110,7 +138,10 @@ const SurveyFirst = ({navigation}) => {
             <View style={{alignItems: 'flex-start', marginHorizontal: 10, marginVertical: 5}}>
                 <View style={{width: '100%'}}>
                     <TextInput placeholder='contoh@gmail.com' style={{borderWidth: 1, borderColor: Color.border,
-                        width: '100%', borderRadius: 5, paddingHorizontal: 10, paddingTop: 20, height: 47}}></TextInput>
+                        width: '100%', borderRadius: 5, paddingHorizontal: 10, paddingTop: 20, height: 47}}
+                        onChangeText={(value) => setEmail(value)}
+                        value={email}
+                    />
                     <Text style={{fontSize: 8, color: Color.secondary, position: 'absolute', paddingHorizontal: 10, paddingVertical: 5}}>Email</Text>
                 </View>
             </View>
@@ -119,14 +150,21 @@ const SurveyFirst = ({navigation}) => {
                 <Text style={{fontSize: 10, fontWeight: 'bold', marginVertical: 10}}>Informasi Petugas</Text>
                 <View style={{width: '100%'}}>
                     <TextInput placeholder='Adang Susanyo' style={{borderWidth: 1, borderColor: Color.border,
-                        width: '100%', borderRadius: 5, paddingHorizontal: 10, paddingTop: 20, height: 47}}></TextInput>
+                        width: '100%', borderRadius: 5, paddingHorizontal: 10, paddingTop: 20, height: 47}}
+                        onChangeText={(value) => setNamePetugas(value)}
+                        value={namePetugas}
+                    />
                     <Text style={{fontSize: 8, color: Color.secondary, position: 'absolute', paddingHorizontal: 10, paddingVertical: 5}}>Nama Petugas</Text>
                 </View>
             </View>
             <View style={{alignItems: 'flex-start', marginHorizontal: 10, marginVertical: 5}}>
                 <View style={{width: '100%'}}>
                     <TextInput placeholder='813-1234-5678' style={{borderWidth: 1, borderColor: Color.border,
-                        width: '100%', borderRadius: 5, paddingHorizontal: 40, paddingTop: 20, height: 47}}></TextInput>
+                        width: '100%', borderRadius: 5, paddingHorizontal: 40, paddingTop: 20, height: 47}}    
+                        keyboardType='number-pad'
+                        onChangeText={(value) => setPhonePetugas(value)}
+                        value={phonePetugas}
+                    />
                     <Text style={{position: 'absolute', marginVertical: 17,marginHorizontal: 10, fontSize: 13}}>+62</Text>
                     <Text style={{fontSize: 8, color: Color.secondary, position: 'absolute', paddingHorizontal: 10, paddingVertical: 5}}>No Telepon</Text>
                 </View>
@@ -134,21 +172,28 @@ const SurveyFirst = ({navigation}) => {
             <View style={{alignItems: 'flex-start', marginHorizontal: 10, marginVertical: 5}}>
                 <View style={{width: '100%'}}>
                     <TextInput placeholder='Adang Susanyo' style={{borderWidth: 1, borderColor: Color.border,
-                        width: '100%', borderRadius: 5, paddingHorizontal: 10, paddingTop: 20, height: 47}}></TextInput>
+                        width: '100%', borderRadius: 5, paddingHorizontal: 10, paddingTop: 20, height: 47}}
+                        onChangeText={(value) => setNameKoor(value)}
+                        value={nameKoor}
+                    />
                     <Text style={{fontSize: 8, color: Color.secondary, position: 'absolute', paddingHorizontal: 10, paddingVertical: 5}}>Nama Koordinator Kab/Kota</Text>
                 </View>
             </View>
             <View style={{alignItems: 'flex-start', marginHorizontal: 10, marginVertical: 5}}>
                 <View style={{width: '100%'}}>
                     <TextInput placeholder='812-1234-5678' style={{borderWidth: 1, borderColor: Color.border,
-                        width: '100%', borderRadius: 5, paddingHorizontal: 40, paddingTop: 20, height: 47}}></TextInput>
+                        width: '100%', borderRadius: 5, paddingHorizontal: 40, paddingTop: 20, height: 47}}
+                        keyboardType='number-pad'
+                        onChangeText={(value) => setPhoneKoor(value)}
+                        value={phoneKoor}
+                    />
                     <Text style={{position: 'absolute', marginVertical: 17,marginHorizontal: 10, fontSize: 13}}>+62</Text>
                     <Text style={{fontSize: 8, color: Color.secondary, position: 'absolute', paddingHorizontal: 10, paddingVertical: 5}}>No. Telepon</Text>
                 </View>
             </View>
         </ScrollView>
         <View style={{width: '100%', height: 70, alignItems: 'center', borderRadius: 10}}>
-            <TouchableOpacity onPress={() => navigation.navigate('SurveySecond')} style={{backgroundColor: Color.primary, width: '90%', height: 45, borderRadius: 50, justifyContent: 'center'}}>
+            <TouchableOpacity onPress={() => submit()} style={{backgroundColor: Color.primary, width: '90%', height: 45, borderRadius: 50, justifyContent: 'center'}}>
                 <Text style={{color: Color.textInput}}>Lanjut</Text>
             </TouchableOpacity>
         </View>
