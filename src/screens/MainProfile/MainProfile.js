deee1b2fb60eed8ff55ac026122e6888c3f58c22
fauzanmd59 <fauzanmd5939@gmail.com>
2022-03-11 06:44:53 +0700

    fix

--- src/screens/MainProfile/MainProfile.js
+++ src/screens/MainProfile/MainProfile.js
@@ -156,7 +156,7 @@ const MainProfile = ({navigation, route}) => {
     {
       code: 'community_admin',
       title: 'Community Admin',
-      show:  user && !user.guest,
+      show: accessClient.MainProfile.showMenuJoinCommunity && user && !user.organizationId,
       icon: <AntDesign name="form" size={20} color={Color.text} style={{}} />,
       onPress: () => navigation.navigate('CommunityAdminPage'),
     },
