5241ec99f5aea06eb34590073315eee1a6a3bb3e
fauzanmd59 <fauzanmd5939@gmail.com>
2022-04-07 21:21:54 +0700

    up

--- src/screens/Ecommerce/MyShopHeader.js
+++ src/screens/Ecommerce/MyShopHeader.js
@@ -1,28 +1,33 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
-import AntDesign from 'react-native-vector-icons/AntDesign'
-import IonIcons from 'react-native-vector-icons/Ionicons'
+import { View, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import IonIcons from 'react-native-vector-icons/Ionicons';
 import { useNavigation } from '@react-navigation/native';
 
 const MyShopHeader = () => {
+	const navigation = useNavigation();
+	return (
+		<View style={{ height: 50, flexDirection: 'row' }}>
+			<TouchableOpacity
+				onPress={() => navigation.goBack()}
+				style={{ width: '10%', alignItems: 'center', justifyContent: 'center' }}
+			>
+				<AntDesign name={'arrowleft'} size={20} />
+			</TouchableOpacity>
+			<View style={{ width: '72%', paddingHorizontal: 10, justifyContent: 'center' }}>
+				<Text style={{ fontSize: 18, fontWeight: 'bold', textAlign: 'left' }}>Toko Saya</Text>
+			</View>
+			<TouchableOpacity
+				style={{ width: '8%', alignItems: 'center', justifyContent: 'center' }}
+				onPress={() => navigation.navigate('ChatRoom')}
+			>
+				<IonIcons name={'chatbubbles-outline'} size={20} />
+			</TouchableOpacity>
+			<View style={{ width: '8%', alignItems: 'center', justifyContent: 'center' }}>
+				<IonIcons name={'notifications-outline'} size={20} />
+			</View>
+		</View>
+	);
+};
 
-  const navigation = useNavigation();
-  return (
-    <View style={{height: 50, flexDirection: 'row'}}>
-      <TouchableOpacity onPress={() => navigation.goBack()} style={{width: '10%', alignItems: 'center', justifyContent: 'center'}}>
-        <AntDesign name={'arrowleft'} size={20}/>
-      </TouchableOpacity>
-      <View style={{width: '72%',paddingHorizontal: 10, justifyContent: 'center'}}>
-        <Text style={{fontSize: 18, fontWeight: 'bold', textAlign: 'left'}}>Toko Saya</Text>
-      </View>
-      <View style={{width: '8%', alignItems: 'center', justifyContent: 'center'}}>
-        <IonIcons name={'chatbubbles-outline'} size={20}/>
-      </View>
-      <View style={{width: '8%', alignItems: 'center', justifyContent: 'center'}}>
-        <IonIcons name={'notifications-outline'} size={20}/>
-      </View>
-    </View>
-  )
-}
-
-export default MyShopHeader
+export default MyShopHeader;
