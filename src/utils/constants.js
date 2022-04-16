ebb38a9f9905a11930308ea87a829568e2a1045c
muhlishabibpradana <muhlis123>
2022-04-16 16:49:45 +0700

    menu home from api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -25,3 +25,109 @@ export const initialItemState = {
     loadNext: false,
     refresh: false,
 };
+
+export const listContentCategory = [
+    {
+        code: 'JOBS',
+        name: 'Loker',
+        nav: 'JobScreen',
+    },
+    {
+        code: 'NEARBY_PLACE',
+        name: 'Tempat',
+        nav: 'PlaceScreen',
+    },
+    {
+        code: 'EVENT',
+        name: 'Event',
+        nav: 'EventScreen',
+    },
+    {
+        code: 'POSTING',
+        name: 'Artikel',
+        nav: 'NewsScreen',
+    },
+    {
+        code: 'FORUM',
+        name: 'Forum',
+        nav: 'ForumScreen',
+    },
+    {
+        code: 'EMERGENCY',
+        name: 'Help Me',
+        nav: '',
+    },
+    {
+        code: 'NEWEST_VIDEO',
+        name: 'Video Terbaru',
+        nav: '',
+    },
+    {
+        code: 'NEWEST_MUSIC',
+        name: 'Musik Terbaru',
+        nav: '',
+    },
+    {
+        code: 'NEWEST_EBOOK',
+        name: 'Buku Terbaru',
+        nav: '',
+    },
+
+    // non
+    {
+        code: 'SURVEY',
+        name: 'Survey',
+        nav: 'SurveyFirst',
+    },
+    {
+        code: 'MEDIA_PLAYER',
+        name: 'Media Player',
+        nav: 'MediaPlayerScreen',
+    },
+    {
+        code: 'E_COMMERCE',
+        name: 'E-Commerce',
+        nav: 'Ecommerce',
+    },
+    {
+        code: 'AUCTION',
+        name: 'Lelang',
+        nav: 'Lelang',
+    },
+    {
+        code: 'PULSA',
+        name: 'Pulsa',
+        nav: 'PulsaScreen',
+    },
+    {
+        code: 'LISTRIK',
+        name: 'Listrik',
+        nav: 'PlnScreen',
+    },
+    {
+        code: 'GAME',
+        name: 'Game',
+        nav: '',
+    },
+    {
+        code: 'PDAM',
+        name: 'PDAM',
+        nav: 'PdamScreen',
+    },
+    {
+        code: 'BPJS',
+        name: 'BPJS',
+        nav: '',
+    },
+    {
+        code: 'INTERNET',
+        name: 'Internet',
+        nav: '',
+    },
+    {
+        code: 'IURAN',
+        name: 'Iuran',
+        nav: 'OrderListPerProduct',
+        params: { title: 'Iuran', type: 'ACTIVE', productType: 'ALL_SAMBATAN' },
+    },
+];
