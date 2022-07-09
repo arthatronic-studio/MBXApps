321f115c5e3b5e312c6570b18f96b070c7f47fec
muhlishabibpradana <muhlis.hp33@gmail.com>
2022-07-09 08:15:08 +0700

    nonactive & delete account (no api)

--- src/screens/MainProfile/SettingScreen.js
+++ src/screens/MainProfile/SettingScreen.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { SafeAreaView, View } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import Fontisto from 'react-native-vector-icons/Fontisto';
@@ -10,20 +10,43 @@ import {
     TouchableOpacity,
     useColor,
     Scaffold,
+    AlertModal,
+    usePopup,
 } from '@src/components';
+import { redirectTo } from 'src/utils';
+import { initSocket } from 'src/api-socket/currentSocket';
+
+const initialAlertModalProps = {
+    visible: false,
+    title: '',
+    message: '',
+    onDiscard: () => {},
+    onSubmit: () => {},
+};
 
 const SettingScreen = ({ navigation, route }) => {
     const { Color } = useColor();
-
     const { theme } = useSelector(state => state.theme);
-    
     const dispatch = useDispatch();
+    const [popupProps, showPopup] = usePopup();
+
+    const [alertModalProps, setAlertModalProps] = useState(initialAlertModalProps);
+
+    const currentSocket = initSocket();
+
+    const onPressLogout = () => {
+        dispatch({type: 'USER.LOGOUT'});
+        redirectTo('LoginScreen');
+        currentSocket.disconnect();
+    };
 
     return (
         <Scaffold
             headerTitle='Pengaturan'
+            popupProps={popupProps}
             onPressLeftButton={() => navigation.navigate('MainProfile', { refresh: true })}
         >
+            {/* dark mode */}
             {/* <Grid style={{backgroundColor: Color.theme, borderTopWidth: 0.5, borderColor: Color.border}}>
                 <Row>
                     <Col align='flex-start' size={8} justify='center'>
@@ -44,7 +67,7 @@ const SettingScreen = ({ navigation, route }) => {
                     </Col>
                 </Row>
             </Grid> */}
-
+            {/* change primary theme color */}
             {/* <Grid style={{backgroundColor: Color.theme, borderTopWidth: 0.5, borderColor: Color.border}}>
                 <Row>
                     <Col align='flex-start' size={8} justify='center'>
@@ -76,21 +99,7 @@ const SettingScreen = ({ navigation, route }) => {
                     </Col>
                 </Row>
             </Grid> */}
-
-            <Grid style={{backgroundColor: Color.theme, borderColor: Color.border}}>
-                <TouchableOpacity
-                    onPress={() => {
-                        navigation.navigate('BlockUser', { canGoBack: true });
-                    }}
-                >
-                    <Row>
-                        <Col align='flex-start' size={8} justify='center'>
-                            <Text size={12} type='medium' style={{marginBottom: 4}}>Daftar Blokir User</Text>
-                            <Text size={8} align='left'>Daftar user yang diblokir oleh anda</Text>
-                        </Col>
-                    </Row>
-                </TouchableOpacity>
-            </Grid>
+            {/* change password */}
             <Grid style={{backgroundColor: Color.theme, borderColor: Color.border}}>
                 <TouchableOpacity
                     onPress={() => {
@@ -109,6 +118,96 @@ const SettingScreen = ({ navigation, route }) => {
                     </Row>
                 </TouchableOpacity>
             </Grid>
+            {/* block list */}
+            <Grid style={{backgroundColor: Color.theme, borderColor: Color.border}}>
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate('BlockUser', { canGoBack: true });
+                    }}
+                >
+                    <Row>
+                        <Col align='flex-start' size={8} justify='center'>
+                            <Text size={12} type='medium' style={{marginBottom: 4}}>Daftar Blokir User</Text>
+                            <Text size={8} align='left'>Daftar user yang diblokir oleh anda</Text>
+                        </Col>
+                        <Col align='flex-end' size={4} justify='center'>
+                            <Fontisto name='arrow-right' size={12} color={Color.text} />
+                        </Col>
+                    </Row>
+                </TouchableOpacity>
+            </Grid>
+            {/* deactivate */}
+            <Grid style={{backgroundColor: Color.theme, borderColor: Color.border}}>
+                <TouchableOpacity
+                    onPress={() => {
+                        setAlertModalProps({
+                            ...alertModalProps,
+                            visible: true,
+                            title: 'Deactive Account',
+                            message: 'Apakah Anda yakin akan menonaktifkan akun sementara?',
+                            onDiscard: () => {
+                                setAlertModalProps(initialAlertModalProps);
+                            },
+                            onSubmit: () => {
+                                setAlertModalProps(initialAlertModalProps);
+                                showPopup('Berhasil nonaktivasi akun', 'success');
+                                setTimeout(() => {
+                                    onPressLogout();
+                                }, 2500);
+                            }
+                        });
+                    }}
+                >
+                    <Row>
+                        <Col align='flex-start' size={8} justify='center'>
+                            <Text size={12} type='medium' style={{marginBottom: 4}}>Deactivate Account</Text>
+                            <Text size={8} align='left'>Deactivating acccount temporary</Text>
+                        </Col>
+                        <Col align='flex-end' size={4} justify='center'>
+                            <Fontisto name='arrow-right' size={12} color={Color.text} />
+                        </Col>
+                    </Row>
+                </TouchableOpacity>
+            </Grid>
+            {/* delete account */}
+            <Grid style={{backgroundColor: Color.theme, borderColor: Color.border}}>
+                <TouchableOpacity
+                    onPress={() => {
+                        setAlertModalProps({
+                            ...alertModalProps,
+                            visible: true,
+                            title: 'Delete Account',
+                            message: 'Apakah Anda yakin akan menghapus akun selamanya?',
+                            onDiscard: () => {
+                                setAlertModalProps(initialAlertModalProps);
+                            },
+                            onSubmit: () => {
+                                setAlertModalProps(initialAlertModalProps);
+                                showPopup('Berhasil menghapus akun', 'success');
+                                setTimeout(() => {
+                                    onPressLogout();
+                                }, 2500);
+                            }
+                        });
+                    }}
+                >
+                    <Row>
+                        <Col align='flex-start' size={8} justify='center'>
+                            <Text size={12} type='medium' color={Color.error} style={{marginBottom: 4}}>Delete Account</Text>
+                            <Text size={8} align='left'>Deleting account permanent</Text>
+                        </Col>
+                        <Col align='flex-end' size={4} justify='center'>
+                            <Fontisto name='arrow-right' size={12} color={Color.text} />
+                        </Col>
+                    </Row>
+                </TouchableOpacity>
+            </Grid>
+
+            <AlertModal
+                {...alertModalProps}
+                type='warning'
+                showDiscardButton
+            />
         </Scaffold>
     );
 }
